89a125f30904250719bb8510871fd58b671c780e
umetsu sunao <s.umetsu11@gmail.com>
2024-10-22 02:39:55 +0900

    deviseでによるログイン機能の修正、初期ユーザーの登録を実行

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'home/show'
 
   root to: "home#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
